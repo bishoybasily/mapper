ef1bd3aa2333e650780b1512f1394bbdd2b60c6d
bishoybasily <bishoybasily@gmail.com>
2020-02-16 04:05:24 +0200

    cleaner content extractor

--- src/main/java/com/ibm/clm/proxy/ProxyApplication.java
+++ src/main/java/com/ibm/clm/proxy/ProxyApplication.java
@@ -10,19 +10,4 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner runner(RabbitTemplate rabbitTemplate){
-//		return args -> {
-//
-//			rabbitTemplate.convertAndSend(
-//					Constants.EXCHANGE,
-//					Constants.CONSUME_ROUTING_KEY,
-//					new Content()
-//							.setUrl("http://checkip.amazonaws.com/")
-//							.setMethod(HttpMethod.GET)
-//			);
-//
-//		};
-//	}
-
 }
